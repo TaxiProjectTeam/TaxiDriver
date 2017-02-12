8bdfb9433ebfd99814a32d6cfb7c0c4c9ad398db
vlads0n <19vlad_son90@bk.ru>
2017-02-12 18:43:06 +0200

    Added fabric crashlytics

--- app/src/main/java/com/example/sveta/taxodriver/activity/LoginActivity.java
+++ app/src/main/java/com/example/sveta/taxodriver/activity/LoginActivity.java
@@ -10,12 +10,14 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.crashlytics.android.Crashlytics;
 import com.example.sveta.taxodriver.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import io.fabric.sdk.android.Fabric;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -29,6 +31,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_login);
 
         auth = FirebaseAuth.getInstance();
